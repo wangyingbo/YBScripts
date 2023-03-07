8b6640eec98c800e767763e0d533d34ac57b57bc
yingbo5 <yingbo5@staff.weibo.com>
2023-03-07 22:41:57 +0800

    code opt

--- install_proxy.sh
+++ install_proxy.sh
@@ -7,9 +7,10 @@ startProxy='' # 开启代理的快捷命令
 endProxy='' # 结束代理的快捷命令
 pathzsh=~/.zshrc
 pathbash=~/.bashrc
+TIME=$(date "+%Y-%m-%d %H:%M:%S")
 _DIR_=`pwd`
-pathzsh=$_DIR_/zshrc.txt
-pathbash=$_DIR_/bashrc.txt
+# pathzsh=$_DIR_/zshrc.txt
+# pathbash=$_DIR_/bashrc.txt
 
 green(){
 	echo -e "\033[32;40m $1 \033[0m"
@@ -25,22 +26,30 @@ read -p "please input shortcut command of start proxy:" startProxy
 if [[ -z $startProxy ]]; then
 	yellow "default start proxy command is 'startproxy'"
 	startProxy='startproxy'
+else
+	green "start proxy command is '$startProxy'"
 fi
 
 read -p "please input shortcut command of end proxy:" endProxy
 if [[ -z $endProxy ]]; then
 	yellow "default end proxy command is 'endproxy'"
 	endProxy='endproxy'
+else
+	green "end proxy command is '$endProxy'"
 fi
 
 read -p "please input proxy port:" port
 if [[ -z $port ]]; then
 	yellow "default proxy port is 7890"
 	port='7890'
+else
+	green "proxy port is '$port'"
 fi
 
+sleep 0.25
 
 genproxy(){
+	echo -e "\n" >> $1
 	cat >> $1 <<PROXYEOF
 PROXYURL=http://127.0.0.1
 PROXYPORT=$port
@@ -51,36 +60,38 @@ alias $endProxy="unset http_proxy https_proxy;curl cip.cc"
 PROXYEOF
 }
 
-bashExist=$(grep 'PROXYURL' $pathbash)
-zshExist=$(grep 'PROXYURL' $pathzsh)
+bashExist=$(grep $startProxy $pathbash)
+zshExist=$(grep $startProxy $pathzsh)
 if [[ -z $zshExist ]]; then
 	echo ""
 else
-	green "\n"
-	green "zsh alias proxy command has existed!"
-	exit 0
+	red "\n"
+	red "zsh alias proxy command has existed!"
+	exit 1
 fi
 if [[ -z $bashExist ]]; then
 	echo ""
 else
-	green "\n"
-	green "bash alias proxy command has existed!"
+	red "\n"
+	red "bash alias proxy command has existed!"
 	exit 0
 fi
 
 if [[ -e $pathzsh ]]; then
 	genproxy $pathbash
 	source $pathbash
+	green "write proxy command to ${pathzsh##*/} successfully!"
 fi
 
 if [[ -e $pathbash ]]; then
 	genproxy $pathzsh
 	source $pathzsh
+	green "write proxy command to ${pathbash##*/} successfully!"
 fi
 
 # shell excute time
 echo ""
-green "($0) ${TIME}: this shell script execution duration: ${SECONDS}s"
+green "${TIME} ($0): this shell script execution duration: ${SECONDS}s"
 echo ""
 
 
